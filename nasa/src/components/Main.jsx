ca8cb4430da57ff60b670b893ae3589e237fad58
Ferencyk Mikhailo <86631675+infernal999@users.noreply.github.com>
2024-06-07 18:14:05 +0200

    fix url if video v2

--- nasa/src/components/Main.jsx
+++ nasa/src/components/Main.jsx
@@ -6,13 +6,13 @@ export default function Main(props) {
   const [toggleImage, setToggleImage] = useState(false);
 
   
-  useEffect(() => {
-    if (
-      data.hdurl.indexOf('image') > -1
-    ) {
-      setToggleImage(toggleImage => !toggleImage)
-    }
-  }, [data])
+  // useEffect(() => {
+  //   if (
+  //     data.hdurl.indexOf('image') > -1
+  //   ) {
+  //     setToggleImage(toggleImage => !toggleImage)
+  //   }
+  // }, [data])
   return (
     <div className="imgContainer">
     {toggleImage ? <IFrameVideo data={data}/> : <img className="bgImage" src={data.hdurl} alt={data.title || 'bg-img'} />}
